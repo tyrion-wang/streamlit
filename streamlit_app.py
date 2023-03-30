09d4fc0fea7049f95f5884a31c6bc88c1989f1eb
Tyrion <maple_leaf_7@msn.com>
2023-03-30 21:20:19 +0800

    Adding files1

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def generate_3(prompt, model, temperature, max_tokens):
 	completions = openai.ChatCompletion.create(
   		model=model,
   		temperature=temperature,
-        max_tokens=max_tokens,
+        max_tokens=2000,
         top_p=1,
 	  	messages=[
 	        {"role": "system", "content": "你是一个作文写作者."},
