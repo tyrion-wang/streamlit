e04286d9a22d6bc6b6be57047fa9a8991fd2722a
Cherry <fuji0701@qq.com>
2023-03-31 02:12:14 +0800

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ def inject_ga():
 
     # Insert the script in the head tag of the static template inside your virtual
     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    st.write(index_path, unsafe_allow_html=True)
     logging.info(f'editing {index_path}')
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
     if not soup.find(id=GA_ID):  # if cannot find tag
